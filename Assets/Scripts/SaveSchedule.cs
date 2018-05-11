365cf3477a8c8ff26e2d61143104c76575c82230
Rahul Shah <rahuldshah3@gmail.com>
2018-05-10 21:26:00 -0400

    add experimental build with working schedule scene

--- Assets/Scripts/SaveSchedule.cs
+++ Assets/Scripts/SaveSchedule.cs
@@ -1,17 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SaveSchedule : MonoBehaviour {
 
+	public InputField fullNameInput;
+	public static string fullName;
+
 	// Use this for initialization
 	void Start () {
-		
+		if (fullName != null) {
+			fullNameInput.text = fullName;
+		}
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	public void SaveUsername(string newName) {
+		fullName = newName;
 	}
 
 
