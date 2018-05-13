1287924c9d16c5c1b23092f740275f8540bd012e
coder4242 <36552319+coder4242@users.noreply.github.com>
2018-05-13 12:59:18 -0400

    Add files via upload
    AutoRotation and PortraitMode

---
+++ PortraitMode.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortraitMode : MonoBehaviour
+{
+
+    // Use this for initialization
+    public void Start()
+    {
+        Screen.orientation = ScreenOrientation.Portrait;
+    }
+
+    // Update is called once per frame
+
+    void Update()
+    {
+
+    }
+
+    //change screen orientation 
+    public void OnClick()
+    {
+        Screen.orientation = ScreenOrientation.LandscapeLeft;
+ 
+    }
+}
